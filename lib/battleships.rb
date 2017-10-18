7294044ff289e22972048facf3d0506db07a494b
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-18 11:01:10 +0100

    Add 2x2 grid and test

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,3 +1,5 @@
 class Battleships
-
+  def grid
+    "0 0\n0 0"
+  end
 end
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -5,4 +5,9 @@ describe 'Battleships' do
     battleships = Battleships.new
     expect(battleships).to be_instance_of(Battleships)
   end
+
+  it "initializes with an array of 4x4 0's" do
+    battleships = Battleships.new
+    expect(battleships.grid).to eq("0 0\n0 0")
+  end
 end
