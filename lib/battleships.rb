702d5d0e40cd406359b352b1225bd4f420394f5c
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-19 11:51:39 +0100

    3rd working test, can add single cell ship

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,5 +1,27 @@
 class Battleships
-  def grid
-    "..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n.........."
+  def create_grid
+    @board = "..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n.........."
+  end
+
+  def place_ship(index)
+    @board[index]= "X"
+    @board
   end
 end
+
+
+#
+# evens = (1..10).each_with_object([]) { |i, a| a << i*2 }
+# #=> [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+#
+# def place_ship(index)
+#   @board[index] = "X"
+# end
+
+# def place_ship(index)
+#   @board[index..(index+1)]= "X"
+# end
+#
+# def place_ship(index)
+#   @board[0,1]= "X"
+# end
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -6,8 +6,14 @@ describe 'Battleships' do
       expect(battleships).to be_instance_of(Battleships)
     end
 
-  it "initializes with an array of 10x10 dots" do
+  it "has grid method to create grid of 10x10 dots" do
     battleships = Battleships.new
-    expect(battleships.grid).to eq('..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........')
+    expect(battleships.create_grid).to eq("..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........")
+  end
+
+  it 'has a method to allow you to place a single cell ship' do
+    battleships = Battleships.new
+    battleships.create_grid
+    expect(battleships.place_ship(0)).to eq("X.........\n..........\n..........\n..........\n..........\n..........\n..........\n..........\n..........")
   end
 end
