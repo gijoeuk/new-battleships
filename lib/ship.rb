b41a8a4c78ca2f6276f54c41d5783b01b7e17d32
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-20 12:58:20 +0100

    Ship knows its co-ordinates

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,7 @@
 class Ship
+  attr_reader :coordinates
 
+  def initialize(index)
+    @coordinates = [index, index +1, index +2]
+  end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,8 +1,12 @@
 require 'Ship'
 
 describe 'Ship' do
-  let( :ship ){ Ship.new }
+  let( :ship ){ Ship.new(1) }
   it 'initializes with an instance of Ship' do
     expect(ship).to be_instance_of(Ship)
+  end
+
+  it 'initializes with three consecutive indices when placed horizontally' do
+    expect(ship.coordinates).to eq([1,2,3])
   end
 end
